9b849de1496e91aab95b2d8dcaa11e142a0efdf2
Mitch1000 <mwdrohan@gmail.com>
2025-07-21 14:49:07 -0400

    removes custom lualine config for backpack color scheme as that is now handled by the colorscheme itself

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,78 +1,18 @@
 local vim = vim
-local palette = require('backpack.colors').setup({ theme = 'dark' }).palette
+GetHighlightColor = require('helpers.get_highlight_color')
 
-local function color(name)
-  return palette[name]
-end
+local status, result = pcall(require, vim.g.my_color_scheme .. [[.colors]])
 
-local colors = {
-  black        = "#262626",
-  dark         = "#222222",
-  bright_white = color("light1"),
-  white        = color("light2"),
-  soft_white   = "#f0f0f0",
-  softy_white   = "#f1e5f0",
-  softer_white = color("light3"),
-  red          = color("dark_red"),
-  green        = color("green"),
-  blue         = color("forest_blue"),
-  blue_light   = "#34a9a7",
-  purple       = color("bright_purple"),
-  yellow       = color("stain_yellow"),
-  gray         = color("light5"),
-  darkgray     = color("dark_gray"),
-  rose         = color("rose"),
-  lightgray    = color("light3"),
-  inactivegray = color("light4"),
-}
-
-local theme = {
-  normal = {
-    a = {bg = colors.blue, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.black, fg = colors.blue},
-    c = {bg = colors.dark, fg = colors.softy_white},
-    x = {bg = colors.dark, fg = colors.soft_white},
-    y = {bg = colors.black, fg = colors.soft_white},
-    z = {bg = colors.blue_light, fg = colors.black, gui = 'bold'},
-  },
-  insert = {
-    a = {bg = colors.purple, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.black, fg = colors.soft_white},
-    c = {bg = colors.dark, fg = colors.white}
-  },
-  visual = {
-    a = {bg = colors.yellow, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.dark, fg = colors.white},
-    c = {bg = colors.dark, fg = colors.black}
-  },
-  replace = {
-    a = {bg = colors.red, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.dark, fg = colors.white},
-    c = {bg = colors.dark, fg = colors.black}
-  },
-  command = {
-    a = {bg = colors.white, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.dark, fg = colors.white},
-    c = {bg = colors.dark, fg = colors.white}
-  },
-  inactive = {
-    a = {bg = colors.darkgray, fg = colors.gray, gui = 'bold'},
-    b = {bg = colors.darkgray, fg = colors.gray},
-    c = {bg = colors.darkgray, fg = colors.gray}
-  }
-}
-
-function GetTheme()
-  if (vim.g.my_color_scheme == 'backpack') then
-    return theme
+local function color(string)
+  if status and vim.g.my_color_scheme == "backpack" then
+    return result.setup({ theme = vim.o.background }).palette[string]
   end
-  return 'auto'
 end
 
 return {
   options = {
      icons_enabled = true,
-     theme = GetTheme(),
+     theme = 'auto',
      component_separators = { left = '', right = ''},
      -- section_separators = { left = '', right = ''},
      section_separators = { left = '', right = ''},
@@ -103,7 +43,7 @@ return {
           colored = true,
           symbols = { error = " ", warn = " ", info = "󱜺 ", hint = "󰺕 " },
           diagnostics_color = {
-            error = { fg = color("dark_red") },
+            error = { fg = color("Error") },
             warn = { fg = color("stain_yellow") },
             info = { fg = color("light3") },
             hint = { fg = color("terminal_blue") },
