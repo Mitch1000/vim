4e90b207b6c171ea34fe2a28d1c6d1787ea39ccb
Mitch1000 <mwdrohan@gmail.com>
2025-07-24 15:51:30 -0400

    prevents errors if coc is not installed by using pcall to call coc function

--- lua/helpers.lua
+++ lua/helpers.lua
@@ -137,7 +137,7 @@ function _G.show_docs()
     if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
         vim.api.nvim_command('h ' .. cw)
     elseif vim.api.nvim_eval('coc#rpc#ready()') then
-        vim.fn.CocActionAsync('doHover')
+        pcall(vim.fn.CocActionAsync, "doHover")
     else
         vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
     end
