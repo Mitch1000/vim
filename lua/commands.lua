61f0e3097eb67268239811d566e784c407339c28
Mitch1000 <mwdrohan@gmail.com>
2025-07-24 16:14:30 -0400

    allows for autochanging ghostty theme with colorscheme background change

--- lua/commands.lua
+++ lua/commands.lua
@@ -100,3 +100,22 @@ end
 
 vim.cmd([[nnoremap zo <cmd>execute "lua OpenFold()"<CR>]])
 vim.cmd([[nnoremap zc <cmd>execute "lua CloseFold()"<CR>]])
+
+if os.getenv("TERM_PROGRAM") == 'ghostty' then
+  local get_initial_bg_color = [[ghostty +show-config | awk -F'= ' '/^background/ {print $2}' | grep "#"]]
+  local initial_bg_color = vim.fn.system(get_initial_bg_color)
+
+
+  function ResetColorScheme()
+    os.execute([[printf '\e]11;]] .. initial_bg_color .. [[\a']])
+  end
+
+  vim.cmd([[autocmd VimLeavePre * execute "lua ResetColorScheme()"]])
+
+  local GetHighlightColor = require('helpers.get_highlight_color')
+  local bg_color = GetHighlightColor("Normal", "guibg")
+  if string.find(bg_color, "#") == nil then
+    bg_color = GetHighlightColor("TelescopeBorder", "guibg")
+  end
+  os.execute([[printf '\e]11;]] .. bg_color .. [[\a']])
+end
