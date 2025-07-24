0f39cff7773f5bb0e104d3329fa7148ed1ccc847
Mitch1000 <mwdrohan@gmail.com>
2025-07-24 15:21:14 -0400

    disables tabs if more than 2 buffers

--- lua/components/handle_tab_icons.lua
+++ lua/components/handle_tab_icons.lua
@@ -1,5 +1,12 @@
 local vim = vim
 return function()
+  local bufinfo = vim.fn.getbufinfo({buflisted = 1})
+  if vim.fn.len(bufinfo) >= 3 then
+    vim.cmd([[BarbarDisable]])
+    vim.cmd([[set showtabline=0]])
+    return
+  end
+
   if Barbar == nil then return end
 
   local function getIcon()
@@ -9,24 +16,28 @@ return function()
     return ''
   end
 
+
   local function getInactive()
-    local bufinfo = vim.fn.getbufinfo({buflisted = 1})
+    bufinfo = vim.fn.getbufinfo({buflisted = 1})
 
     if vim.fn.len(bufinfo) < 3 then
-      vim.cmd([[hi StatusLineNC guifg=none guibg=none]])
+      -- vim.cmd([[hi StatusLineNC guifg=none guibg=none]])
+      return
     else
-      vim.cmd([[hi StatusLineNC guifg=none guibg=#121212]])
+      -- vim.cmd([[hi StatusLineNC guifg=none guibg=#121212]])
+      vim.cmd([[BarbarDisable]])
+      vim.cmd([[set showtabline=0]])
     end
 
-    if vim.fn.len(bufinfo) < 3 and vim.fn.bufnr('%') == 1 then
-      return ''
-    end
+    --if vim.fn.len(bufinfo) < 3 and vim.fn.bufnr('%') == 1 then
+    --  return ''
+    --end
 
 
     return ''
   end
 
-  Barbar.setup({
+  local updated_config =  {
     icons = {
 
  		  pinned = {
@@ -49,5 +60,9 @@ return function()
       },
       separator_at_end = true
     },
-  })
+  }
+  local barbar_config = require('config.barbarConf')
+  for k,v in pairs(updated_config) do barbar_config[k] = v end
+
+  Barbar.setup(barbar_config)
 end
