c1f9f48bcbf2af7fe4b5a5934ec8e6a66d72a064
Mitch1000 <mwdrohan@gmail.com>
2025-07-28 20:03:26 -0400

    allows plugins config to set background

--- lua/globals.lua
+++ lua/globals.lua
@@ -1,8 +1,6 @@
 local vim = vim
 
 vim.opt.statuscolumn = " %s%{v:lnum}"
--- vim.g.my_color_scheme = 'backpack'
-vim.o.background = 'dark'
 vim.g.signify_sign_add = '┃'
 vim.g.signify_sign_change = '┃'
 vim.g.signify_sign_delete = '•'
