c5cc23e15e3d5a762292a3ad418119523aab278a
Mitch1000 <mwdrohan@gmail.com>
2025-07-28 20:02:33 -0400

    removes commented commands

--- lua/commands.lua
+++ lua/commands.lua
@@ -20,8 +20,6 @@ cmd([[command! GL :lua GitLoad()]])
 cmd([[command! GB :lua GitBlame()]])
 cmd([[command! CONS :lua PrintJavascriptConsoleLogs()]])
 cmd([[command! CL :lua vim.fn.setreg("a", '')]])
--- cmd([[command! BD :set background=dark]])
--- cmd([[command! BL :set background=light]])
 
 -- Go to next linter error
 cmd([[command! AN call CocAction('diagnosticNext')]])
@@ -36,16 +34,6 @@ cmd([[command! RL source $MYVIMRC | lua ReloadLightLine()]])
 cmd([[command! -range CC <line1>,<line2>s/\(_\)\(.\)/\u\2/g]])
 cmd([[command! -range CamelCase <line1>,<line2>s/\(_\)\(.\)/\u\2/g]])
 
--- Define auto commands
-
--- cmd([[autocmd OptionSet background
---       \ execute 'source' '~/.config/nvim/bundle/backpack/autoload/lightline/colorscheme/backpack.vim'
---       \ | call lightline#colorscheme() | call lightline#update()]])
--- cmd([[autocmd TermEnter * execute "highlight LineNr guifg=" .. g:background_color[0] ]])
--- cmd([[autocmd TermEnter * execute "highlight LineNr guibg=" .. g:background_color[0] ]])
--- cmd([[autocmd TermEnter * execute "highlight CursorLineNr guifg=" .. g:background_color[0] ]])
--- cmd([[autocmd TermLeave * execute "highlight LineNr guifg=" .. g:line_nr[0] ]])
--- cmd([[autocmd TermLeave * execute "highlight CursorLineNr guifg=" .. g:line_nr[0] ]])
 
 cmd([[autocmd BufRead,BufNewFile *.vue setlocal filetype=vue.html.javascript.css || TSBufDisable highlight]])
 
