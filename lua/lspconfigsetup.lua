afc87166fcd2a6cb4429c62f7f32f63d2949067d
Mitch1000 <mwdrohan@gmail.com>
2025-11-04 16:09:50 -0500

    modernizes lspconfig

--- lua/lspconfigsetup.lua
+++ lua/lspconfigsetup.lua
@@ -1,33 +1,57 @@
  -- Language Server Config Setups
-local status, lspconfig = pcall(require, 'lspconfig')
-if not status then
-   return
-end
+local vim = vim
+local lspconfig = vim.lsp.config
 
-lspconfig.volar.setup({
-   init_options = {
-     --typescript = {
-     --  tsdk = "/Users/dirtplantman/.nvm/versions/node/v22.14.0/lib/node_modules/typescript/lib",
-     --}
+local vue_language_server_path = "/Users/mog/.nvm/versions/node/v22.17.0/bin/vue-language-server"
+local ts_language_server_path = "/Users/mog/.nvm/versions/node/v22.17.0/bin/typescript-language-server"
+local ts_lib_path = "/Users/mog/.nvm/versions/node/v22.17.0/lib/node_modules/typescript-language-server/lib"
+
+lspconfig.ts_ls = {
+  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+  --init_options = {
+
+  --  --plugins = {
+  --  --  {
+  --  --    name = "@vue/typescript-plugin",
+  --  --    location = vue_language_server_path,
+  --  --    languages = { "vue" },
+  --  --  },
+  --  --},
+  --},
+}
+
+lspconfig.vue_ls = {
+  cmd = vue_language_server_path,
+  init_options = {
+   vue = {
+     hybridMode = false,
    },
-})
+   -- NOTE: This might not be needed. Uncomment if you encounter issues.
 
-lspconfig.java_language_server.setup{}
-lspconfig.pyright.setup{}
-lspconfig.html.setup{}
-lspconfig.sqls.setup{}
-lspconfig.ts_ls.setup{}
-lspconfig.rubocop.setup{}
-lspconfig.volar.setup{}
-lspconfig.ccls.setup{}
-lspconfig.rust_analyzer.setup{}
-lspconfig.ts_query_ls.setup{}
-lspconfig.tailwindcss.setup{}
+   --typescript = {
+   --  tsdk = ts_lib_path,
+   --},
+  },
+ }
+            
+vim.lsp.enable('ts_ls')
+vim.lsp.enable('vue_ls')
 
+local language_servers = {
+  "java_language_server",
+  "pyright",
+  "html",
+  "sqls",
+  "rubocop",
+  "ccls",
+  "rust_analyzer",
+  "ts_query_ls",
+  "tailwindcss",
+  "cssls",
+  "eslint"
+}
 
--- lspconfig.ltex.setup({
---   -- on_attach = on_attach,
---   cmd = { "ltex-ls" },
---   filetypes = { "markdown", "text" },
---   flags = { debounce_text_changes = 300 },
--- })
+for _index, value in ipairs(language_servers) do
+  lspconfig[value] = {}
+  vim.lsp.enable(value)
+end
